f7db1ed993f86bd24cea03d07f2079fe7eca5396
suheyl <suheyl@brandver.se>
2022-07-07 14:38:32 +0500

    file download, csv data added

--- src/country/language.controller.ts
+++ src/country/language.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get, Param, Post, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { Controller, Get, Param, Post, Res, StreamableFile, UploadedFiles, UseGuards, UseInterceptors } from '@nestjs/common';
+import { StreamableFileOptions } from '@nestjs/common/file-stream/streamable-options.interface';
 import { FilesInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
+import { AuthGuard } from 'src/auth.guard';
 import { ImporterService } from 'src/storage/importer.service';
+import { Stream } from 'stream';
 import { LanguageService } from './language.service';
 
 @Controller('languages')
 export class LangagesController {
-  constructor(private readonly countryService: LanguageService, private readonly importer: ImporterService) { }
+  constructor(private readonly languages: LanguageService, private readonly importer: ImporterService) { }
   
 
   @Post()
@@ -25,6 +29,19 @@ export class LangagesController {
 
   @Get(':code')
   async get(@Param('code') code: string) {
-    return await this.countryService.get(code)
+    return await this.languages.get(code)
+  }
+
+  @Get('download/:type')
+  async download(@Param('type') type: string, @Res() res: Response) {
+    console.log('download requested')
+    const languages = await this.languages.all()
+
+    const stream = new Stream.Readable()
+    for (const language of languages) {
+      stream.push(`${JSON.stringify(language)}\r\n`)
+    }
+    stream.push(null)
+    return new StreamableFile(stream)
   }
 }
--- src/country/language.service.ts
+++ src/country/language.service.ts
@@ -2,8 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { ISO639 } from 'src/storage/entities/iso_639.entity';
 import { DataSource, Repository } from 'typeorm';
 
+export type LanguageType = {
+  code2: string
+  language: string
+  native: string
+}
+
 @Injectable()
 export class LanguageService {
+
   private readonly _rep: Repository<ISO639>;
   constructor(private readonly ds: DataSource) {
     this._rep = ds.getRepository(ISO639)
@@ -17,5 +24,23 @@ export class LanguageService {
     })
 
     return row
+  }
+  async all() {
+    const rows = await this._rep.find({
+      select: ["code2", "country", "native"],
+      order: {
+        code2: 'ASC'
+      }
+    })
+
+    const languages = rows.map(function (x: ISO639) {
+      return {
+        code2: x.code2,
+        language: x.country,
+        native: x.native
+      } as LanguageType         
+    })
+    
+    return languages
   }
 }
